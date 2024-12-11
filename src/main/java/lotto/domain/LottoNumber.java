0f9817188150e39dccd489dcf58541fb0c552fae
이창근 <dlckdrms0517@gmail.com>
2024-12-11 15:09:31 +0900

    feat: 무작위 로또 숫자 생성 기능 개발

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,6 +1,9 @@
 package lotto.domain;
 
 import lotto.common.exception.CustomExceptions;
+import lotto.service.numberPicker.NumberPicker;
+
+import java.util.List;
 
 public class LottoNumber {
 	
@@ -18,5 +21,12 @@ public class LottoNumber {
 		if (number < MIN_NUMBER || number > MAX_NUMBER) {
 			throw CustomExceptions.INVALID_LOTTO_NUMBER_RANGE.get(MIN_NUMBER, MAX_NUMBER);
 		}
+	}
+	
+	public static List<LottoNumber> createUnique(int count, NumberPicker numberPicker) {
+		List<Integer> numbers = numberPicker.pickNumberInRange(MIN_NUMBER, MAX_NUMBER, count);
+		return numbers.stream()
+				.map(LottoNumber::new)
+				.toList();
 	}
 }
